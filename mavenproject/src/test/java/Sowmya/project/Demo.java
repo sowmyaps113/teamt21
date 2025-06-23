a710acbeae50ae9be2cfb40cfcf2807662721adb
admin <admin@DESKTOP-3VI02IA>
2025-06-23 15:27:24 +0530

    New Class Demo added

---
+++ mavenproject/src/test/java/Sowmya/project/Demo.java
@@ -0,0 +1,13 @@
+package Sowmya.project;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		System.out.println("This");
+		System.out.println("is");
+		System.out.println("Sowmya");
+	}
+
+}
